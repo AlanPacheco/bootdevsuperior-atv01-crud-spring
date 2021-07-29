e2f3977f9d303a24b5c0ca371ad4e4278c380aaf
Alan Pacheco <alan.estudo@gmail.com>
2021-07-28 23:27:27 -0300

    Create project backend and configuration database h2

---
+++ backend/src/main/java/com/alanph/devsuperiorresourceclient/DevsuperiorResourceclientApplication.java
@@ -0,0 +1,13 @@
+package com.alanph.devsuperiorresourceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorResourceclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorResourceclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alanph/devsuperiorresourceclient/DevsuperiorResourceclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanph.devsuperiorresourceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorResourceclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
