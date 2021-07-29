1e2621790eb576534055b3bf61cb2548905d5bfb
Alan Pacheco <alan.estudo@gmail.com>
2021-07-28 23:49:03 -0300

    Add ClientRepository

---
+++ backend/src/main/java/com/alanph/devsuperiorresourceclient/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.alanph.devsuperiorresourceclient.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alanph.devsuperiorresourceclient.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
