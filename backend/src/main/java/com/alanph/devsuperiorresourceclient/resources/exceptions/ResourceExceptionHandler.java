709dedbb9b2514826b0c31cb18e26ab043a9c812
Alan Pacheco <alan.estudo@gmail.com>
2021-07-30 22:17:23 -0300

    Add endpoint findById of Client and @ControllerAdvice

---
+++ backend/src/main/java/com/alanph/devsuperiorresourceclient/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.alanph.devsuperiorresourceclient.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.alanph.devsuperiorresourceclient.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException resourceNotFound, HttpServletRequest request){
+		StandardError standardError = new StandardError();
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		standardError.setTimestamp(Instant.now());
+		standardError.setStatus(status.value());
+		standardError.setError("Resource Not Found");
+		standardError.setMessage(resourceNotFound.getMessage());
+		standardError.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(standardError);
+	}
+}
--- backend/src/main/java/com/alanph/devsuperiorresourceclient/services/ClientService.java
+++ backend/src/main/java/com/alanph/devsuperiorresourceclient/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.alanph.devsuperiorresourceclient.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.alanph.devsuperiorresourceclient.dto.ClientDTO;
 import com.alanph.devsuperiorresourceclient.entities.Client;
 import com.alanph.devsuperiorresourceclient.repositories.ClientRepository;
+import com.alanph.devsuperiorresourceclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,6 +23,15 @@ public class ClientService {
 		Page<Client> pageClient = clientRepository.findAll(pageRequest);
 		Page<ClientDTO> pageClientDTO = pageClient.map(e -> new ClientDTO(e));
 		return pageClientDTO;
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> optionalClient = clientRepository.findById(id);
+		Client client = optionalClient.orElseThrow(() -> new ResourceNotFoundException("Id not found"));
+		ClientDTO clientDTO = new ClientDTO(client);
+		return clientDTO;
+		
 	}
 
 }
